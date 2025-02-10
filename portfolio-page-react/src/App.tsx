cc538a77b9c719595d1ab1dbb4cbdaeb9c5c601d
Rene Reede <rene.reede@gmail.com>
2025-02-10 20:37:47 +0200

    Feat: Improve phone view
    Make Main button flexible

--- portfolio-page-react/src/App.tsx
+++ portfolio-page-react/src/App.tsx
@@ -15,11 +15,11 @@ function App() {
 <h1 className='text-3xl'>Hi, I'm Rene, a front-end web developer specializing in JavaScript and React.</h1>
 
 <div className="left-bottom-container mr-10 text-center">
-     <Link to='/project'><button className='bg-blue-500 px-6 py-3 rounded-2xl text-white font-bold w-sm'>Click to see my latest webshop project!<br/><small>(Built with ReactJS, TypeScript and Tailwind)</small></button></Link>
+     <Link to='/project'><button className='bg-blue-500 px-6 py-3 rounded-2xl text-white font-bold w-sm portfolio-project-btn'>Click to see my latest webshop project!<br/><small>(Built with ReactJS, TypeScript and Tailwind)</small></button></Link>
 
 <div className="links mt-6 w-full">
-  <a href="https://www.linkedin.com/in/rene-reede/" target='_blank'><button className='mt-6 bg-gray-700 rounded-md text-white py-3 px-3 mr-6'>LinkedIn</button></a>
-  <a href="https://github.com/rreede" target='_blank'><button className='mt-3 bg-gray-700 rounded-md text-white py-3 px-3'>GitHub</button></a>
+  <a href="https://www.linkedin.com/in/rene-reede/" target='_blank'><button className='mt-6 bg-gray-700 rounded-md text-white py-3 px-3 mr-6 link-btn'>LinkedIn</button></a>
+  <a href="https://github.com/rreede" target='_blank'><button className='mt-3 bg-gray-700 rounded-md text-white py-3 px-3 link-btn'>GitHub</button></a>
 </div>
 </div>
 </div>
